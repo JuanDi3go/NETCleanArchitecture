6aa80c400622098a0548e59ca6ee20724211640a
Juan Diego Aguilar Angel <Juandiegoaguilarok@gmail.com>
2023-04-30 09:36:22 -0500

    CreateOrderiteractor, CreateOrderInputPort, CreteOrderOutputPort

---
+++ NorthWind.Sales.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -0,0 +1,26 @@
+﻿
+namespace NorthWind.Sales.UseCases.CreateOrder
+{
+    public class CreateOrderInteractor : ICreateOrderInputPort
+    {
+        readonly ICreateOrderOutputPort _outPutPort;
+        readonly INorthWindSalesCommandsRepository _repository;
+
+        public CreateOrderInteractor(ICreateOrderOutputPort outPutPort, INorthWindSalesCommandsRepository repository)
+        {
+            _outPutPort = outPutPort;
+            _repository = repository;
+        }
+
+        public async ValueTask Handle(CreateOrderDTO orderDTO)
+        {
+
+            OrderAggregate Order = OrderAggregate.From(orderDTO);
+
+            await _repository.CreateOrder(Order);
+            await _repository.SaveChanges();
+            await _outPutPort.Handle(Order.Id);
+
+        }
+    }
+}
